f705572e1668bcb0643bad65768ca8e3b92c8668
jowell <87069318+JoelDigbeu@users.noreply.github.com>
2022-08-24 12:38:25 +0000

    implement user CRUD

--- apps/api/src/features/user/service/user.service.ts
+++ apps/api/src/features/user/service/user.service.ts
@@ -1,26 +1,52 @@
+import { SharedPrismaService } from '@nest-graphql-cqrs/shared/prisma'
 import { Injectable } from '@nestjs/common'
 import { CreateUserInput } from '../dto/create-user.input'
 import { UpdateUserInput } from '../dto/update-user.input'
 
 @Injectable()
 export class UserService {
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user'
+  constructor(private prismaService: SharedPrismaService) {}
+
+  async create(createUserInput: CreateUserInput) {
+    const user = await this.prismaService.user.create({
+      data: {
+        ...createUserInput,
+      },
+    })
+
+    return user
   }
 
-  findAll() {
-    return `This action returns all user`
+  async findAll() {
+    return await this.prismaService.user.findMany({
+      include: {
+        articles: true,
+      },
+    })
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`
+  async findOne(id: number) {
+    return await this.prismaService.user.findMany({
+      where: { id },
+      include: {
+        articles: true,
+      },
+    })
   }
 
-  update(id: number, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`
+  update(updateUserInput: UpdateUserInput) {
+    const { id, ...rest } = updateUserInput
+    return this.prismaService.user.update({
+      where: { id },
+      data: {
+        ...rest,
+      },
+    })
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`
+    return this.prismaService.user.delete({
+      where: { id },
+    })
   }
 }
--- apps/api/src/features/user/user.module.ts
+++ apps/api/src/features/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { UserService } from './service/user.service'
 import { UserResolver } from './resolver/user.resolver'
+import { SharedPrismaModule } from '@nest-graphql-cqrs/shared/prisma'
 
 @Module({
+  imports: [SharedPrismaModule],
   providers: [UserResolver, UserService],
 })
 export class UserModule {}
